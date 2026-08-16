fef5bec6898a45ea158e2215a151911d60e8861d
AndreLiar <andrelaurelyvan.kanmegnetabouguie@ynov.com>
2026-08-16 19:07:53 +0200

    fix: move pool.Close() defer after migration early exit (gocritic exitAfterDefer)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -44,12 +44,12 @@ func main() {
 			log.Error("failed to create db pool", "err", err)
 			os.Exit(1)
 		}
-		defer pool.Close()
-
 		if err := runMigrations(pool, log); err != nil {
+			pool.Close()
 			log.Error("migration failed", "err", err)
 			os.Exit(1)
 		}
+		defer pool.Close()
 
 		svc = domain.NewPolicyService(pgRepo.NewPolicyRepo(pool))
 	} else {
